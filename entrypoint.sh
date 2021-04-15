8d356883efdfaed3d270037db49470ac6b481890
Brook Keele <1662976+x-a-n-d-e-r-k@users.noreply.github.com>
2021-04-14 23:39:16 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 
-cd "/app/"
+cd /app/
 
 cat /github/workspace/*.js > /github/workspace/all_scripts.js
 npx minify /github/workspace/all_scripts.js --out-file /github/workspace/all_scripts_min.js
